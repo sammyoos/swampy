587ad9c78f2c34bd3e18b4d1fe5e53d5ed2b2574
sammyoos <sammyo@gmail.com>
2015-02-18 21:13:43 -0500

    OK base canvas working with first quarter of football showing

---
+++ README.txt
@@ -0,0 +1,9 @@
+
+
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes
+
+https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/A_basic_ray-caster
+
+Canvas Size
+http://stackoverflow.com/questions/19666790/html5-canvas-100-height-and-width
+
--- swampy.js
+++ swampy.js
@@ -1,10 +1,82 @@
-function main() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+var canvas;
+var ctx; // context for canvas
 
-    ctx.fillRect(25,25,100,100);
-    ctx.clearRect(45,45,60,60);
-    ctx.strokeRect(50,50,50,50);
+var mode = 0;
+var maxMode = 1;
+
+var maxIncs = 100;
+var i = 0;
+
+var footballMode = 0;
+var maxFootballMode = 4;
+
+function resetEdges()
+{
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+
+
+function clearScreen ( ctx, canvas )
+{
+	ctx.fillStyle = "rgba(0,0,0,0.8)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawFootball()
+{
+    ctx.beginPath();
+	ctx.moveTo( i * canvas.width / maxIncs, 0 );
+	ctx.lineTo( 0, canvas.height - i * canvas.height / maxIncs );
+    ctx.closePath();
+	ctx.stroke();
+}
+
+function resetEdges()
+{
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+
+
+function clearScreen()
+{
+	ctx.fillStyle = "rgba(0,0,0,0.8)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+
+function main() {
+  canvas = document.getElementById('swamp');
+  if (canvas.getContext) {
+    ctx = canvas.getContext('2d');
+
+	resetEdges();
+	clearScreen();
+
+	setInterval( updateLines, 30 );
   }
+}
+
+function updateLines()
+{
+	switch( mode ){
+		case 0:
+			drawFootball();
+			break;
+		default:
+			// oh oh
+	}
+	
+	if( ++i >= maxIncs )
+	{
+		i = 0;
+		++mode;
+
+		if( mode >= maxMode )
+		{
+			mode = 0;
+		}
+	}
+
 }
